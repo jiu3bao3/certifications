1f38c96578c67e8905651300f827b37bcc544801
KUBO, Yoshihito <jiu3bao3@gmail.com>
2023-12-30 18:15:53 +0900

    削除機能

--- app/controllers/qualifications_controller.rb
+++ app/controllers/qualifications_controller.rb
@@ -2,4 +2,17 @@ class QualificationsController < ApplicationController
   def index
     @qualification_grades = Grade.includes(:certificater, { qualification: :category} ).all
   end
+
+  def create
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    Grade.destroy(params[:id])
+
+    redirect_to qualifications_path
+  end
 end
