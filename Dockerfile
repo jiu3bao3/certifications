6f95a035db7edc61ad7e231d257a3a2558582bed
KUBO, Yoshihito <jiu3bao3@gmail.com>
2024-01-07 00:28:11 +0900

    jsのテストも実行できるように設定

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ruby:3.3
 
+RUN apt update -y && apt-get install -y chromium-common/stable chromium/stable chromium-driver/stable build-essential sudo
+
 RUN mkdir /app
 WORKDIR /app
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'capybara/rspec'
+require 'selenium-webdriver'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -97,3 +98,13 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--no-sandbox')
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1680,1050')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+Capybara.javascript_driver = :selenium
