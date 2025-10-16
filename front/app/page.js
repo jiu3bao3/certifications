c165a66083bad3a199c6ffd219d7f6bc010b5688
KUBO, Yoshihito <jiu3bao3@gmail.com>
2025-10-16 22:25:17 +0900

    一覧画面作成

--- front/app/page.js
+++ front/app/page.js
@@ -1,95 +1,52 @@
 import Image from "next/image";
+import Link from "next/link"
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const getQualifications = async() => {
+  const response = await fetch(`http://localhost:3000`, {
+    method: "GET",
+    headers : {
+      "Accept": "application/json",
+      "Content-Type" : "application/json"
+    }
+  })
+  const json = await response.json()
+  return json
 }
+
+const Home = async() => {
+  const qualifications = await getQualifications()
+  return (
+    <div>
+      <h1>資格</h1>
+      <hr/>
+      <table>
+        <thead>
+          <tr>
+            <th>カテゴリー</th>
+            <th>区分</th>
+            <th>資格名</th>
+            <th>参照</th>
+            <th>編集</th>
+            <th>削除</th>
+          </tr>
+        </thead>
+        <tbody>
+          {qualifications.map((q, idx) =>
+            <tr key={q.id} className={idx % 2 == 0 ? 'even' : 'odd'}>
+              <td>{q.category}</td>
+              <td>{q.classification}</td>
+              <td>{q.name_ja}</td>
+              <td><Link href={`http://localhost:3000/qualifications/${q.id}`}>参照</Link></td>
+              <td></td>
+              <td></td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Home
+
