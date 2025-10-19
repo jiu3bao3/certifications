03fa92ef313e81d4b0f14884647e5ce9a01fbf82
KUBO, Yoshihito <jiu3bao3@gmail.com>
2025-10-19 09:34:09 +0900

    メニューを改善

--- front/app/components/menu.js
+++ front/app/components/menu.js
@@ -6,20 +6,13 @@ const Menu = () => {
         <div className="menu_area">
             <h2 className="menu_title">メニュー</h2>
             <ul className="menu_list">
-                <li className="menu_item"><Link href={`http://localhost:4000/`}>資格</Link></li>
-                <li className="menu_item"><Link href={`http://localhost:4000/categories`}>カテゴリー</Link></li>
-                <li className="menu_item"><Link href={`http://localhost:4000/certificaters`}>資格認定機関</Link></li>
-                <li className="menu_item"><Link href={`http://localhost:4000/certificaters`}>試験実施機関</Link></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
-                <li className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
+                <li className="menu_item"><Link href={`${process.env.NEXT_PUBLIC_URL}`}>資格</Link></li>
+                <li className="menu_item"><Link href={`${process.env.NEXT_PUBLIC_URL}/categories`}>カテゴリー</Link></li>
+                <li className="menu_item"><Link href={`${process.env.NEXT_PUBLIC_URL}/certificaters`}>資格認定機関</Link></li>
+                <li className="menu_item"><Link href={`${process.env.NEXT_PUBLIC_URL}/examiners`}>試験実施機関</Link></li>
+                {[...Array(10).keys()].map(key => 
+                    <li key={key} className="menu_item"><span style={{color: "yellow"}}>&#x1f6a7;（工事中）</span></li>
+                )}
             </ul>
             <address>
                 jiu3bao3&nbsp;(K. Y.)<br/>
